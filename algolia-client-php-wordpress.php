b31626e7e9a5dca57d9b89cd7405defa66a3d803
Yoshiteru Nagata <nagata.yoshiteru@g.mbox.nagoya-u.ac.jp>
2019-09-04 14:25:01 +0900

    added api-client

---
+++ algolia-client-php-wordpress.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name:     Algolia Client PHP WordPress
+ * Description:     Add Algolia Search feature to WordPress
+ * Text Domain:     algolia-client-php-wordpress
+ * Version:         0.0.1
+ * Author:          Y.Nagata
+ *
+ * @package         Algolia_Client_PHP_WordPress
+ */
+
+// Your code starts here.
